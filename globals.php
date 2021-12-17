42947f6060efeb1dab9da1a68b61ce629cf7f2a5
Joni Spets <joni.spets@edu.sasky.fi>
2021-12-17 15:50:20 +0200

    Corinthia fontti lisätty

--- globals.php
+++ globals.php
@@ -25,7 +25,10 @@
       "url"  => "https://fonts.googleapis.com/css2?family=Sigmar+One&display=swap"),
     "titanone" => array(
       "name" => "Titan One",
-      "url"  => "https://fonts.googleapis.com/css2?family=Titan+One&display=swap")
+      "url"  => "https://fonts.googleapis.com/css2?family=Titan+One&display=swap"),
+    "corinthia" => array(
+      "name" => "Corinthia",
+      "url"  => "https://fonts.googleapis.com/css2?family=Bakbak+One&family=Corinthia&display=swap")
   );
 
   // Otsakkeen taustat aakkosjärjestyksessä.
